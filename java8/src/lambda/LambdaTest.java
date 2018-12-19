af4e0b4f0c4d2714b36e7372bc49104abc78c210
innerpeacez <zhaihw0417@163.com>
2018-12-19 13:53:36 +0800

    Function

--- java8/src/lambda/LambdaTest.java
+++ java8/src/lambda/LambdaTest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
@@ -28,12 +29,21 @@ public class LambdaTest {
 //        List list = filter(strings, filter);
 //        System.out.println(list);
 
-        Consumer<Integer> integerConsumer = (Integer t) -> System.out.println(t);
-        forEach(Arrays.asList(1, 2, 3, 4, 5), integerConsumer);
+//        Consumer<Integer> integerConsumer = (Integer t) -> System.out.println(t);
+//        forEach(Arrays.asList(1, 2, 3, 4, 5), integerConsumer);
+
+        List<Integer> map = map(Arrays.asList("zhai", "hong", "wei"), (String::length));
+        System.out.println(map);
     }
 
     public static <T> void forEach(List<T> t, Consumer<T> c) {
         t.forEach(i -> c.accept(i));
+    }
+
+    public static <T, R> List<R> map(List<T> list, Function<T, R> f) {
+        List<R> result = new ArrayList<>();
+        list.forEach(v -> result.add(f.apply(v)));
+        return result;
     }
 
     public static <T> List<T> filter(List<T> list, Predicate p) {
