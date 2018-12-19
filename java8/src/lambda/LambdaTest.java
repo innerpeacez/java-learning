96db6dcaae2cabcf1e961c3bb817a9c0e1cfe01c
innerpeacez <zhaihw0417@163.com>
2018-12-19 12:04:01 +0800

    lambda

--- java8/src/lambda/LambdaTest.java
+++ java8/src/lambda/LambdaTest.java
@@ -1,5 +1,10 @@
 package lambda;
 
+import java.nio.file.DirectoryStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 /**
  * @author innerpeacez
  * @since 2018/12/18
@@ -12,6 +17,23 @@ public class LambdaTest {
 
 
     public static void main(String[] args) {
-        process(() -> System.out.println("Hello World Lambda"));
+//        process(() -> System.out.println("Hello World Lambda"));
+
+        Predicate<String> filter = (String s) -> !s.isEmpty();
+        List strings = new ArrayList<>();
+        strings.add("zhw");
+        strings.add("");
+        List list = filter(strings, filter);
+        System.out.println(list);
+    }
+
+    public static <T> List<T> filter(List<T> list, Predicate p) {
+        List results = new ArrayList();
+        list.forEach((T) -> {
+            if (p.test(T)) {
+                results.add(T);
+            }
+        });
+        return results;
     }
 }
