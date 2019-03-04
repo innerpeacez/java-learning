27e9bc35019238d5ace0ccbcedd7f6ffea726f46
innerpeacez <zhaihw0417@163.com>
2019-03-04 20:09:51 +0800

    stream 流操作

--- java-8/src/test/java/com/zhw/java/study/stream/StreamTest.java
+++ java-8/src/test/java/com/zhw/java/study/stream/StreamTest.java
@@ -41,4 +41,85 @@ public class StreamTest {
         System.out.println("---------------");
         menu.parallelStream().forEach(System.out::println);
     }
+
+    @Test
+    public void testDistinct() {
+        List<Integer> integers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+        integers.stream()
+                .filter(i -> i % 2 == 0)
+                .distinct()
+                .forEach(System.out::println);
+
+
+    }
+
+    @Test
+    public void testWords() {
+        List<String> words = Arrays.asList("Java 8", "In", "Action");
+        words.stream()
+                .map(String::length)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void dishNameLength() {
+        menu.stream()
+                .map(Dish::getName)
+                .map(String::length)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void testGetDiffChart() {
+        List<String> words = Arrays.asList("HELLO", "WORLD");
+        words.stream()
+                .map(word -> word.split(""))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList()).stream()
+                .distinct()
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void testSquare() {
+        Arrays.asList(1, 2, 3, 4, 5).stream()
+                .map(num -> num * num)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+
+
+    @Test
+    public void testDuishu() {
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        List<Integer> list1 = Arrays.asList(3, 4);
+        list.stream()
+                .flatMap(i -> list1.stream().map(j -> new int[]{i, j}))
+                .collect(Collectors.toList());
+
+    }
+
+    @Test
+    public void testDuishu2() {
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        List<Integer> list1 = Arrays.asList(3, 4);
+        list.stream()
+                .flatMap(i -> list1.stream()
+                        .filter(j -> (i + j) % 2 == 0)
+                        .map(j -> new int[]{i, j}))
+                .collect(Collectors.toList());
+
+
+    }
+
+    @Test
+    public void firstNum3() {
+        Arrays.asList(1, 2, 3, 4, 5).stream()
+                .map(num -> num * num)
+                .filter(num -> num % 3 == 0)
+                .findFirst()
+                .ifPresent(System.out::println);
+    }
 }
